6c15f6f511a42866400bbff60f00c96efaaad3a8
Nicolas <nicolas.six@insa-lyon.fr>
2017-02-17 08:59:29 +0100

    start of Lexer class

--- Sources/Lexer.cpp
+++ Sources/Lexer.cpp
@@ -1,5 +1,95 @@
-//
-// Created by francois on 16/02/17.
-//
-
 #include "Lexer.h"
+#include <iostream>
+
+Lexer::Lexer()
+{
+    currentSymbol = nullptr;
+}
+
+Lexer::~Lexer()
+{
+
+}
+
+int Lexer::getCharType(char c) const
+{
+    if(c == '+')
+    {
+        return PLUS;
+    }
+    else if(c == '*')
+    {
+        return MULT;
+    }
+    else if(c == '(')
+    {
+        return OPEN;
+    }
+    else if(c == ')')
+    {
+        return CLOSE;
+    }
+    else if(c <= '9' && c >= '0')
+    {
+        return NUMBER;
+    }
+    else if(c == EOF)
+    {
+        return END;
+    }
+}
+
+std::string Lexer::nextWord()
+{
+    std::string word = "";
+    char currentChar;
+
+    currentChar = std::cin.peek();
+
+    while(getCharType(currentChar) == UNKNOWN)
+    {
+        std::cin.get();
+        currentChar = std::cin.peek();
+    }
+
+    while(getCharType(currentChar) == NUMBER)
+    {
+        word.append(currentChar);
+        std::cin.get();
+        currentChar = std::cin.peek();
+    }
+
+    if(word == "")
+    {
+        word.append(currentChar);
+        std::cin.get();
+    }
+
+    return word;
+}
+
+void Lexer::getNextSymbole()
+{
+    std::string word;
+
+    word = nextWord();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Sources/Lexer.h
+++ Sources/Lexer.h
@@ -5,9 +5,34 @@
 #ifndef GRAMMAIRELANGAGE_LEXER_H
 #define GRAMMAIRELANGAGE_LEXER_H
 
+#include <string>
+#include <memory>
+#include "Symbol.h"
 
 class Lexer {
 
+public:
+    Lexer();
+    ~Lexer();
+
+    Symbol get();
+    Symbol peek();
+
+protected:
+    void getNextSymbole();
+    std::string nextWord();
+    int getCharType(char c) const;
+
+private:
+    std::shared_ptr<Symbol> currentSymbol;
+
+    const int PLUS    = 1;
+    const int MULT    = 2;
+    const int OPEN    = 3;
+    const int CLOSE   = 4;
+    const int NUMBER  = 5;
+    const int END     = 6;
+    const int UNKNOWN = -1;
 };
 
 
