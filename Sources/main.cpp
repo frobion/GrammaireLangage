fafab957c361d1dacdea522501a15f055f702968
Nicolas <nicolas.six@insa-lyon.fr>
2017-02-20 18:22:25 +0100

    loop on lecture until Ctrl+C

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -10,14 +10,12 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     cout << "enter an expression:" << endl;
-    Automate automate;
-    automate.lecture();
-//    Lexer lexer;
-//    std::shared_ptr<Symbol> symbol;
-//    do{
-//        symbol = lexer.get();
-//        cout << *symbol << endl;
-//    }while (*symbol != ID_EOF);
+    //loop on lecture
+    while(true)
+    {
+        Automate automate;
+        automate.lecture();
+    }
 
     return 0;
 }
